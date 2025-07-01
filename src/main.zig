d4884d56a3a87f097f35ef5a19dd1b060e633f22
HirschBerge <hirschy.kirkwood@proton.me>
2025-07-01 18:50:22 -0400

    Runs mpv with a url in a child process.

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,19 @@
 //! By convention, main.zig is where your main function lives in the case that
 //! you are building an executable. If you are making a library, the convention
 //! is to delete this file and start with root.zig instead.
+const ChildProcess = std.process.Child;
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const result = try ChildProcess.run(.{
+        .allocator = std.heap.page_allocator,
+        .argv = &[_][]const u8{
+            "mpv",
+            "https://www.youtube.com/watch?v=3pdkMH52Wls",
+        },
+    });
+    std.debug.print("{s}\n", .{result.stdout});
 
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
