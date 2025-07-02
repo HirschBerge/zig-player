657f5e672f17bad4c0d462eb5ae0f8a3e95b898d
HirschBerge <hirschy.kirkwood@proton.me>
2025-07-02 15:39:42 -0400

    Refactored to new func

--- src/main.zig
+++ src/main.zig
@@ -4,43 +4,23 @@
 
 pub fn main() !void {
     const clip_contents = try clip_utils.read();
-    const argv = [_][]const u8{
+    try play_video(clip_contents);
+}
+
+fn play_video(url: []const u8) !void {
+    const argv = [3][]const u8{
         "mpv",
-        clip_contents,
+        url,
         "--no-terminal",
     };
-    // try clipboard.write("Zig ⚡");
-    // std.debug.print("{s}\n", .{clip_contents catch "Nothing to see here"});
     var child = std.process.Child.init(&argv, std.heap.page_allocator);
     try child.spawn();
-    const exit_code = child.wait();
-    try std.testing.expectEqual(exit_code, std.process.Child.Term{ .Exited = 0 });
+    // const exit_code = child.wait();
+    // try std.testing.expectEqual(exit_code, std.process.Child.Term{ .Exited = 0 });
 }
 
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
 
 const std = @import("std");
 /// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("zig_player_lib");
+// const lib = @import("zig_player_lib");
 const clip_utils = @import("clipboard");
